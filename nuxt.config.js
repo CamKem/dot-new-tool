1d07d22f42be99aadb11834023ef19654d606be3
Cameron Kemshal-Bell <cam@panr.app>
2024-03-11 10:00:43 +1100

    Installing Pinia

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,11 +30,12 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@pinia/nuxt'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
