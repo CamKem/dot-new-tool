18d1847bf35f6608dde2bb9147b8b815eab25075
Cameron Kemshal-Bell <cam@panr.app>
2024-03-12 01:19:18 +1100

    Only check once per reload if the table name exists

--- database/db.js
+++ database/db.js
@@ -1,7 +1,16 @@
 import Database from 'better-sqlite3';
 import fs from 'fs';
 const db = new Database('./database/db.sqlite', { verbose: console.log });
-const schema = fs.readFileSync('./database/schema.sql', 'utf8');
-// create table
-db.exec(schema);
+
+// check if the records table exists
+const tableExists = db.prepare("SELECT name FROM sqlite_master WHERE type='table' AND name='records';").get();
+
+if (!tableExists) {
+  // if the records table does not exist, create it
+  const schema = fs.readFileSync('./database/schema.sql', 'utf8');
+  // create table
+  db.exec(schema);
+}
+
+
 export default db;
