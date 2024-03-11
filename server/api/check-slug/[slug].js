b04dff0da35cef8e97fc1a1170cd851b12ff544a
Cameron Kemshal-Bell <cam@panr.app>
2024-03-11 23:13:00 +1100

    case sensitivity added

--- server/api/check-slug/[slug].js
+++ server/api/check-slug/[slug].js
@@ -3,6 +3,6 @@ import db from '../../../database/db';
 export default defineEventHandler(async (event) => {
   const slug = getRouterParam(event, 'slug');
   const stmt = db.prepare('SELECT slug FROM records WHERE slug = ?');
-  const record = await stmt.get(slug);
+  const record = await stmt.get(slug.toLowerCase());
   return { exists: !!record };
 });
